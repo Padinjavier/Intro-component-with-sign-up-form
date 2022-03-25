3876f568fb81ec4d641f169f6887c430462123c9
javier <javierpadin661@gmail.com>
2022-03-25 18:56:29 -0500

    identifica si los datos son los solicitados o no

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,40 +16,49 @@ const cuatro = document.querySelector(".entrada4");
 var error4 = document.querySelector(".error4");
 var error44 = document.querySelector(".error44");
 
+btn.addEventListener("click", (e) => {
+	e.preventDefault();
+	if (/^[a-zA-Z]+([a-zA-Z])*$/.test(uno.value)) {
+		error1.classList.remove("active");
+ 		error11.classList.remove("active");
+	} else {
+        error1.classList.add("active");
+ 		error11.classList.add("active");
+	}
 
-const toggleElement = (element, nameClass) => {
-	element.classList.toggle(nameClass)
-}
+    if (/^[a-zA-Z]+([a-zA-Z])*$/.test(dos.value)) {
+		error2.classList.remove("active");
+ 		error22.classList.remove("active");
+	} else {
+         error2.classList.add("active");
+ 		error22.classList.add("active");
+	}
 
-btn.addEventListener('click', () => {
-	var dato1 = document.getElementsByClassName("entrada1")[0].value;
-    if(dato1==""){
-        toggleElement(error1, 'active')
-        toggleElement(error11, 'active')
-    }
-    var dato2 = document.getElementsByClassName("entrada2")[0].value;
-    if(dato2==""){
-        toggleElement(error2, 'active')
-        toggleElement(error22, 'active')
-        // dos.value = "llena la casilla imbecil";
-    }
-    var dato3 = document.getElementsByClassName("entrada3")[0].value;
-    if(dato3==""){
-        toggleElement(error3, 'active')
-        toggleElement(error33, 'active')
+	if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(tres.value)) {
+    	error3.classList.remove("active");
+    	error33.classList.remove("active");
+    } else {
+    	error3.classList.add("active");
+    	error33.classList.add("active");
         tres.value = "email@example/com";
         document.querySelector(".entrada3").style.color = 'red';
     }
+
     var dato4 = document.getElementsByClassName("entrada4")[0].value;
-    if(dato4==""){
-        toggleElement(error4, 'active')
-        toggleElement(error44, 'active')
-        // cuatro.value = "llena la casilla imbecil";
+	if (dato4=="") {
+		error4.classList.add("active");
+ 		error44.classList.add("active");
+	} else {
+		error4.classList.remove("active");
+ 		error44.classList.remove("active");
+	}
+});
+
+// al dar click en la entrada de correo se borra el mensaje
+tres.addEventListener('click', () =>{
+    var dato3 = document.getElementsByClassName("entrada3")[0].value;
+    if (dato3=="email@example/com") {
+        tres.value = "";
+        document.querySelector(".entrada3").style.color = 'black';
     }
 })
-
-tres.addEventListener('click', () =>{
-    tres.value = "";
-    document.querySelector(".entrada3").style.color = 'black';
-})
-
